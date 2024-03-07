b2a3b251892cc76232f662fa7fa38d3e6e4e607d
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-03-07 21:32:18 +0200

    reverted

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const UserInvestment = require('./models/userInvestment');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
